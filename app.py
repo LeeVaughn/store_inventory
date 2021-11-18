01e8d1dc07270833cb6be60b8d6aff1e61272838
LeeVaughn <dlvprogramming@gmail.com>
2021-11-17 21:59:53 -0800

    Create Product class

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from peewee import *
 
 db = SqliteDatabase("inventory.db")
+
+
+class Product(Model):
+    product_id = AutoField()
+    product_name = CharField()
+    product_quantity = IntegerField()
+    product_price = IntegerField()
+    date_updated = DateTimeField()
+
+    class Meta():
+        database = db
