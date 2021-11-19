b56e824000514e46e4afed52c4c8fa489f886678
LeeVaughn <dlvprogramming@gmail.com>
2021-11-18 20:06:46 -0800

    Add function to read CSV and add items to DB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from peewee import *
+import csv
+import pprint
 
 db = SqliteDatabase("inventory.db")
 
@@ -13,6 +15,27 @@ class Product(Model):
     class Meta():
         database = db
 
+def add_products():
+    with open("inventory.csv", newline="") as csvfile:
+        inventory = csv.DictReader(csvfile, delimiter=",")
+        
+        for product in inventory:
+            # converts string excluding $ to equivalent of cents
+            price_in_cents = int(float(product["product_price"][1:]) * 100)
+
+            try:
+                Product.create(
+                    product_name=product["product_name"],
+                    product_quantity=product["product_quantity"],
+                    product_price=price_in_cents,
+                    date_updated=product["date_updated"]
+                )
+            except IntegrityError:
+                product_record = Product.get(product_name=product["product_name"])
+                product_record.points = product["points"]
+                product_record.save()
+
 if __name__ == "__main__":
     db.connect()
     db.create_tables([Product], safe=True)
+    add_products()
